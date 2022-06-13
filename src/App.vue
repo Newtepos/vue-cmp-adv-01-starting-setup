7040c5613a0d01cf8adca9b150a70dc88adf3578
Supakit <supakit.pnm@gmail.com>
2022-06-13 21:55:15 +0700

    Create: ManageGoal and ActiveGoal Component

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,57 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
+    <!-- <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
     <course-goal #default="slotProps">
-        <h2>{{ slotProps.item }}</h2>
-        <p>{{ slotProps["anotherProp"]}}</p>
-    </course-goal>
+      <h2>{{ slotProps.item }}</h2>
+      <p>{{ slotProps["anotherProp"] }}</p>
+    </course-goal> -->
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goal v-if="selectedComponent === 'manage-goals'"></manage-goal> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import TheHeader from './components/TheHeader.vue'
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoal from './components/CourseGoal.vue';
+import TheHeader from "./components/TheHeader.vue";
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+// import CourseGoal from './components/CourseGoal.vue';
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoal.vue";
 
 export default {
   components: {
-    'the-header': TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoal
+    "the-header": TheHeader,
+    // BadgeList,
+    // UserInfo,
+    // CourseGoal,
+    ActiveGoals,
+    ManageGoals,
   },
   data() {
     return {
+      selectedComponent: "active-goals",
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
       },
     };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
   },
 };
 </script>
